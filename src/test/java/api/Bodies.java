0c0a1b816b480d5d922a1876644c63971db1bd13
IlyaGrushevskiy <belpochta05@gmail.com>
2021-04-13 10:46:14 +0300

    add two tests: "add to cart" and "add review"

---
+++ src/test/java/api/Bodies.java
@@ -0,0 +1,13 @@
+package api;
+
+public class Bodies {
+
+    public String bodyComputer() {
+        return "product_attribute_16_5_4=14&product_attribute_16_6_5=15&product_attribute_16_3_6=18&product_attribute_16_4_7=44&product_attribute_16_8_8=22&addtocart_16.EnteredQuantity=1";
+    }
+
+    public String bodyReview() {
+        return "AddProductReview.Title=Super&AddProductReview.ReviewText=Super+Power&AddProductReview.Rating=5&add-review=Submit+review";
+    }
+
+}
---
+++ src/test/java/tests/Tests.java
@@ -0,0 +1,62 @@
+package tests;
+
+import api.Auth;
+import api.Bodies;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+public class Tests extends BaseTest {
+
+    Bodies bodies = new Bodies();
+
+    @Test
+    @DisplayName("Add to product cart")
+    public void addToCart() {
+        Map<String, String> cookies = new Auth().login("qaguru@qa.guru", "qaguru@qa.guru1");
+
+        String body = bodies.bodyComputer();
+
+        Response response = given()
+                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                .cookies(cookies)
+                .body(body)
+                .when()
+                .post("/addproducttocart/details/16/1")
+                .then()
+                .statusCode(200)
+                .log().body()
+                .body("success", is(true))
+                .extract().response();
+
+
+        System.out.println(response.statusCode());
+    }
+
+    @Test
+    @DisplayName("Add product review")
+    public void addProductReview() {
+        Map<String, String> cookies = new Auth().login("qaguru@qa.guru", "qaguru@qa.guru1");
+
+        String body = bodies.bodyComputer();
+
+        Response response = given()
+                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                .cookies(cookies)
+                .body(body)
+                .when()
+                .post("/productreviews/32")
+                .then()
+                .statusCode(200)
+                .log().body()
+                .extract().response();
+
+        System.out.println(response.statusCode());
+    }
+
+}
